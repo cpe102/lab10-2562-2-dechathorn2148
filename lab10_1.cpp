178bad4339a35acbab680783de050ad2d703a714
lab314-18@labeng.local <lab314-18@labeng.local>
2020-01-31 17:34:06 +0700

    finished

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,48 @@
 // cout << "Press Enter 3 times to reveal your future.";
 // cout << "You will get A in this 261102.";
+#include <iostream>
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+	cout << "Press Enter 3 times to reveal your future.";
+	cin.ignore();
+	cin.ignore();
+	cin.ignore();
+	int i;
+	string s;
+	i = rand()%9 + 1;
+	switch (i)
+	{
+		case 1 :
+			s = "A";
+			break;
+		case 2 :
+			s = "B+";
+			break;
+		case 3 :
+			s = "B";
+			break;
+		case 4 :
+			s = "C+";
+			break;
+		case 5 :
+			s = "C";
+			break;
+		case 6 :
+			s = "D+";
+			break;
+		case 7 :
+			s = "D";
+			break;
+		case 8 :
+			s = "F";
+			break;
+		case 9 :
+			s = "W";
+			break;							
+	}
+	cout << "You will get " << s << " in this 261102.";
+}
